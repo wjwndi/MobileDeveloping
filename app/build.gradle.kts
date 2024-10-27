6a5bda2f056c54cb45e1b68f312b8f59563d4d97
wjwndi <sofialevina2005@gmail.com>
2024-10-27 21:09:51 +0300

    lab work 8 initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.coil.compose)
+
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
